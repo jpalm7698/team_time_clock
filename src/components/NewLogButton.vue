94404963a2661efa373681abc7fd8140d6675dd1
James Palmer <jpalmer7698@gmail.com>
2022-01-21 11:49:41 -0600

    feat: 'New Log' button in Log Entries view

---
+++ src/components/NewLogButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <b-container>
+            <b-button pill variant="primary" size="lg">New Log</b-button>
+        </b-container>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'NewLogButton',
+}
+</script>
--- src/views/hours-logs/index.vue
+++ src/views/hours-logs/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div id="page-contents">
-    <LogsTable/>
-    </div>
+  <b-container fluid>
+    <b-row align-v="start">
+      <b-col cols="1">
+        <NewLogButton/>
+      </b-col>
+      <b-col>
+        <LogsTable/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import NewLogButton from '../../components/NewLogButton.vue'
 import LogsTable from '../../components/LogsTable.vue'
 
 export default {
-  components: { LogsTable },
+  components: { NewLogButton, LogsTable },
 }
 </script>

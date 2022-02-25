1773af3589f1139b6361cef5f107afd8ccefee24
James Palmer <jpalmer7698@gmail.com>
2022-02-24 21:49:08 -0600

    Add firebase, analytics, auth, and auth emulator

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
+// Create Firebase App Object
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirebaseConfig } from "./firebase-config";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Initialize Firebase
+const firebaseConfig = getFirebaseConfig();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+connectAuthEmulator(auth, "http://localhost:4400");
+
+if (app && analytics && auth) {
+  console.log('Google Firebase Services were setup successfully.');
+}
+
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
